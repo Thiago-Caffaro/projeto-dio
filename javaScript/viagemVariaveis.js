11312e4b4fa0af2d6bfccceec9620e5f19616c93
Thiago-Caffaro <96150887+Thiago-Caffaro@users.noreply.github.com>
2022-11-06 14:21:29 -0300

    Pasta sobre JS
    Adicionado a pasta javaScript Juntamente com um exercício.

---
+++ javaScript/viagemVariaveis.js
@@ -0,0 +1,14 @@
+//Faça um programa para calcular o valor de uma viagem.
+//Você terá 3 variáveis. Sendo elas:
+//1 - Preço do combustível;
+//2 - Gasto médio de combustível do carro por KM;
+//3 - Distância em KM da viagem;
+//Imprima no console o valor que será gasto de combustível para realizar esta viagem.
+
+const precoLitro = 6.32;
+const distancia = 80;
+const kmPorLitro = 10;
+
+let litroConsumido = distancia / kmPorLitro;
+let gasto = litroConsumido * precoLitro;
+console.log(gasto);
