a0b48ce37e70d18cd6872401bfbd0e8abf72fbec
Thiago-Caffaro <96150887+Thiago-Caffaro@users.noreply.github.com>
2023-04-09 23:06:32 -0300

    Update testes.js

--- javaScript/testes.js
+++ javaScript/testes.js
@@ -1,4 +1,13 @@
-let i = 0;
-for (i; i < 10; i++){
-	console.log(i);
+let elementos = [64, 137, -16, 43, 67, 81, -90, 212, 10, 75]; 
+
+let valor = 81;
+let achou = "nao"
+for (let p=0; p <= elementos.length; p++){
+  if (elementos[p] === valor){
+    console.log("Achei " + elementos[p] +" na posicao " + elementos.indexOf(elementos[p]))
+    achou = "sim"
+  }
+}
+if (achou == "nao"){
+	console.log("Numero " + valor + " nao encontrado!")
 }
